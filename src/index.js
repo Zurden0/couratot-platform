a7e28ae66e4d38c1bc336f20b7feecb2e2d9579a
Zurden0 <lene.gertim19@gmail.com>
2026-06-09 13:55:49 +0700

    feat: добавил router страниц

--- src/index.js
+++ src/index.js
@@ -8,25 +8,46 @@ import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import GeneralUserInfo from "./components/GeneralUserInfo";
 
+import Router from "./router";
+import { navigate } from "./router";
+
+function ProfilePage() {
+  // сделать валидацию на авторизацию
+  return (
+    <Content>
+      <Sidebar />
+      <div className="main-content">
+        <Header>
+          <GeneralUserInfo
+            full_name="Герман"
+            login="@german"
+            bio_content="Пользователь еще не оставил информацию о себе..."
+          />
+        </Header>
+      </div>
+    </Content>
+  )
+}
+
+function NotFoundPage() {
+  return (
+    <div>404 — Страница не найдена</div>
+  )
+}
+
 const body = ReactDOM.createRoot(document.querySelector('body'));
 body.render(
   <React.StrictMode>
-      {
-        <Content>
-          <Sidebar />
-          <div className={"main-content"}>
-            <Header>
-              <GeneralUserInfo
-                full_name={"Герман"}
-                login={"@german"}
-                bio_content={"Пользователь еще не оставил информацию о себе..."}
-              />
-            </Header>
-          </div>
-        </Content>
-      }
+    <Router
+      routes={{
+        "/profile": <ProfilePage />,
+        "*": <NotFoundPage />,
+      }}
+    />
   </React.StrictMode>
 );
+
+
 
 
 reportWebVitals();

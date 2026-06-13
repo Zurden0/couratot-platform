07e4ea2edae968a361c75a830916edeb0664d677
Zurden0 <lene.gertim19@gmail.com>
2026-06-13 09:53:43 +0700

    feat: объединил автора и дату статьи в один параграф

--- src/components/Article.js
+++ src/components/Article.js
@@ -15,15 +15,6 @@ export const getArticlesData = () => {
       creator: "Тучина Нина Васильевна",
       createdAT: "2026-06-12 12:45", // "YYYY-MM-DD HH:MM"
       endAt: "2026-06-22 22:45"
-    },
-    {
-      imgLinks: {
-        previewImgID: 0, // номер массива
-        images: ["/storage/articlesImg/salad.jpg", "/storage/articlesImg/salad.jpg", "/storage/articlesImg/salad.jpg"]
-      },
-      title: "Тестовая c очень длинным названием но не более 100 символов",
-      text: "Contrary to popular belief, <br>Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n",
-      createdAT: "2026-10-06, 11:11"
     }
   ]
 }
@@ -115,8 +106,7 @@ const Article = ({articleData}) => {
         <p className="title">{articleData.title}</p>
 
         <div>
-          <p className="creator">{articleData.creator}</p>
-          <p className="date">{articleData.createdAT}</p>
+          <p className="creator-and-date">{articleData.creator}, {articleData.createdAT}</p>
 
           <p className={`end-timer ${deltaBool}`}>
             {deltaText}

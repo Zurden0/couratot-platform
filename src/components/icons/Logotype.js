9c203d204479701de3534db44a241c07b6d912cd
Zurden0 <lene.gertim19@gmail.com>
2026-06-15 15:56:54 +0700

    feat: добавил ссылку на главную страницу в логотип

--- src/components/icons/Logotype.js
+++ src/components/icons/Logotype.js
@@ -1,8 +1,12 @@
+import {navigate} from "../../router";
+
 const Logotype = () => {
   return (
     <img
       src="/favicon.png"
       alt="aiec"
+      onClick={() => navigate("/")}
+      style={{cursor: "pointer"}}
     />)
 }
 

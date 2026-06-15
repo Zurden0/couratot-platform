d9f56171204338114ff9c4f2f92e6a3ffd7eb36a
Zurden0 <lene.gertim19@gmail.com>
2026-06-15 15:57:30 +0700

    feat: убрал статичное поле "войти" из nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import {navigate} from "../router";
 const createNavOption = (navOption) => {
   return (
     <>
-      <a onClick={() => navigate(navOption.url)}>{navOption.name}</a>
+      <a className={navOption.className ?? ""} onClick={() => navigate(navOption.url)}>{navOption.name}</a>
     </>
   )
 }
@@ -13,7 +13,6 @@ const Nav = ({navOptionsList}) => {
   return (
     <div className={"nav"}>
       {navOptionsList.map(element => createNavOption(element))}
-      <a className={"login"} onClick={() => navigate('/login')}>Войти</a>
     </div>
   )
 }
